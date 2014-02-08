fc8232495b6092914acb91c85502b67d047e2521
Stephen M. Cameron <stephenmcameron@gmail.com>
2014-02-08 16:18:07 -0600

    add lines

--- mister-nouveau-artboto.py
+++ mister-nouveau-artboto.py
@@ -22,16 +22,53 @@
 
 import sys
 import Image
+import ImageDraw
 import math
+import random
 
 xdim = 2048;
 ydim = 1024;
+maxspeed = 0.10;
+minspeed = 0.001;
+maxpenwidth = xdim / 30;
+minpenwidth = xdim / 400;
 
 outputimg = Image.new("RGB", (xdim, ydim));
+draw = ImageDraw.Draw(outputimg);
 
-for x in range(0, int(xdim)):
-   for y in range(0, int(ydim)):
-      outputimg.putpixel((x, y), (255, 255, 255));
+def clear_image():
+   for x in range(0, int(xdim)):
+      for y in range(0, int(ydim)):
+         outputimg.putpixel((x, y), (255, 255, 255));
+
+class pen:
+   def __init__(self):
+      self.x = random.random() * xdim;
+      self.y = 1.0 * ydim;
+      self.direction = 90 * math.pi / 180.0;
+      self.direction += (((random.random() * 2.0) - 1.0) * 10 * math.pi / 180.0);
+      self.speed = random.random() * (maxspeed - minspeed) + minspeed;
+      self.angle = (((random.random() * 2.0) - 1.0) * 30 * math.pi / 180.0);
+      self.width = random.random() * random.random() * (maxpenwidth - minpenwidth) + minpenwidth;
+
+   def move(self):
+      self.x = self.x + math.cos(self.direction) * self.speed;
+      self.y = self.y - math.sin(self.direction) * self.speed;
+
+   def paint(self):
+      x1 = self.x + math.cos(self.angle) * self.width / 2.0;
+      y1 = self.y + math.sin(self.angle) * self.width / 2.0;
+      x2 = self.x - math.cos(self.angle) * self.width / 2.0;
+      y2 = self.y - math.sin(self.angle) * self.width / 2.0;
+      draw.line([(int(x1), int(y1)), (int(x2), int(y2))], fill=0);
+
+clear_image();
+
+for i in range(20):
+   p = pen();
+   for j in range(45000):
+      p.move();
+      p.paint();
 
 outputimg.save("output.png");
 
